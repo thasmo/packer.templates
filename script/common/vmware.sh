1ff5a1fb0e6150824afff5436c9641908686eaf8
Thomas Deinhamer <thasmo@gmail.com>
2013-11-18 00:30:02 +0100

    Remove obsolete fix for VMware 9

--- script/common/vmware.sh
+++ script/common/vmware.sh
@@ -12,13 +12,6 @@ tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 umount /mnt/vmware
 rm -f /home/vagrant/linux.iso
 
-# Apply patch
-cd /tmp/vmware-tools-distrib/
-wget --no-check-certificate https://raw.github.com/ebdevrepo/bin/master/vmware9.compat_mm.patch
-wget --no-check-certificate https://raw.github.com/ebdevrepo/bin/master/vmware_hgfs_fix.sh
-chmod +x vmware_hgfs_fix.sh
-./vmware_hgfs_fix.sh
-
 # Execute setup
 /tmp/vmware-tools-distrib/vmware-install.pl --default
 
