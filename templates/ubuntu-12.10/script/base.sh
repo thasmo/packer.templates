b62ed2513b599b122758b5ed82d626ae2c75b834
Thomas Deinhamer <thasmo@gmail.com>
2013-11-17 22:02:48 +0100

    Restructure directory layout

--- templates/ubuntu-12.10/script/base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Update the box
-apt-get -y update
-apt-get -y upgrade
-
-# Install dependencies
-apt-get -y install dkms
-apt-get -y install nfs-common
--- templates/ubuntu-13.10/script/builder/virtualbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /mnt/virtualbox
-mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
-sh /mnt/virtualbox/VBoxLinuxAdditions.run
-umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
